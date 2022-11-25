73fbd9e9ba94d2552527c2272e7bb702ec7deae1
Monika <dotyk_zla@o2.pl>
2022-11-25 22:17:09 +0100

    LAB3 - EX3 - Create employee

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,7 +1,8 @@
-import {Observable} from "rxjs";
+import {map, Observable} from "rxjs";
 import {PersonModel} from "../model/person.model";
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
+import {CreateEmployeeModel} from "../model/create-employee.model";
 
 @Injectable()
 export class EmployeeService {
@@ -10,5 +11,9 @@ export class EmployeeService {
 
   getAll(): Observable<PersonModel[]> {
     return this._httpClient.get<PersonModel[]>('/assets/data/people.json');
+  }
+
+  create(employee:CreateEmployeeModel):Observable<void> {
+    return this._httpClient.post('https://jsonplaceholder.typicode.com/posts', employee).pipe(map(_ => void 0));
   }
 }
--- src/app/ui/employee-form/employee-form.component.ts
+++ src/app/ui/employee-form/employee-form.component.ts
@@ -1,5 +1,7 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { EmployeeService } from '../../services/employee.service';
+import { CreateEmployeeModel } from '../../model/create-employee.model';
 
 @Component({
   selector: 'app-employee-form',
@@ -9,13 +11,22 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 })
 export class EmployeeFormComponent {
   readonly employeeForm: FormGroup = new FormGroup(
-    { email: new FormControl(null, [Validators.required]),
-            age: new FormControl(null, [Validators.min(0), Validators.required]),
-            salary: new FormControl(null, [Validators.min(0), Validators.required])
+    {
+      name: new FormControl(null, [Validators.required]),
+      age: new FormControl(null, [Validators.min(0), Validators.required]),
+      salary: new FormControl(null, [Validators.min(0), Validators.required])
     }
   )
 
-  onButtonClicked(form: { email : string, age: number, salary: number}) {
-    alert(`User was successfully added to the database! Email: ${form.email}, Age: ${form.age}, Salary: ${form.salary}`)
+  constructor(private _employeeService: EmployeeService) {
   }
+
+  onButtonClicked(form: { name: string, age: number, salary: string }) {
+    alert(`User was successfully added to the database! Email: ${form.name}, Age: ${form.age}, Salary: ${form.salary}`)
+  }
+
+  onFormSubmitted(form: CreateEmployeeModel) {
+    this._employeeService.create({ name: form.name, age: form.age, salary: form.salary }).subscribe()
+    this.onButtonClicked(form)
+    }
 }

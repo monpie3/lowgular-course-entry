b7f987c3d4dabb6e8c23c6d81b4f7bcedaa703cf
Monika <dotyk_zla@o2.pl>
2022-11-26 17:36:00 +0100

    LAB3 - EX5 - Map Response to Model

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -3,21 +3,38 @@ import {PersonModel} from "../model/person.model";
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {CreateEmployeeModel} from "../model/create-employee.model";
+import {ApiResponse} from "./api.response";
+import {EmployeeResponse} from "./employee.response";
 
 @Injectable()
 export class EmployeeService {
   constructor(private _httpClient: HttpClient) {
   }
 
+  // https://dummy.restapiexample.com/
+
   getAll(): Observable<PersonModel[]> {
-    return this._httpClient.get<PersonModel[]>('/assets/data/people.json');
+    return this._httpClient.get<ApiResponse<EmployeeResponse[]>>(
+      'https://dummy.restapiexample.com/api/v1/employees',
+      ).pipe(
+        map((response:ApiResponse<EmployeeResponse[]>) => {
+          return response.data.map((employeeResponse: EmployeeResponse) => {
+            return {
+              personalNumber: employeeResponse.id,
+              name: employeeResponse.employee_name,
+              email: employeeResponse.employee_name.replace(/\s+/g, '_').toLowerCase() + '@lowgular.io',
+              img: employeeResponse.employee_image
+            }
+          });
+      })
+    )
   }
 
   create(employee:CreateEmployeeModel):Observable<void> {
-    return this._httpClient.post('https://jsonplaceholder.typicode.com/posts', employee).pipe(map(_ => void 0));
+    return this._httpClient.post('https://dummy.restapiexample.com/api/v1/create', employee).pipe(map(_ => void 0));
   }
 
   delete(id:string):Observable<void> {
-    return this._httpClient.delete('https://jsonplaceholder.typicode.com/posts/' + id).pipe(map(_ => void 0));
+    return this._httpClient.delete('https://dummy.restapiexample.com/api/v1/delete/2' + id).pipe(map(_ => void 0));
   }
 }
